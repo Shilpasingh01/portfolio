bb440965d51022a12fb3ee1c76538c1a180e13b8
Shilpasingh01 <shilpasingh691@gmail.com>
2023-06-30 02:56:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let nav = document.querySelector(".navbar");
+    window.onscroll = function () {
+        if(document.documentElement.scrollTop > 20){
+            nav.classList.add("header-scrolled");
+        }else{
+            nav.classList.remove("header-scrolled");
+        }
+    } 
+    let navBar = document.querySelectorAll(".nav-link");
+    let navCollapse = document.querySelector(".navbar-collapse.collapse");
+    navBar.forEach(function(a){
+        a.addEventListener("click",function(){
+            navCollapse.classList.remove("show");
+     })
+ })
+
+
+    ScrollReveal({
+        reset:true,
+        distance:'80px',
+        duration:2000,
+        delay:200,
+
+    });
+
+    ScrollReveal().reveal('.banner_wrapper,.portfolio_wrapper',{origin:'top'});
+    ScrollReveal().reveal('.service_wrapper,.blog_wrapper',{origin:'bottom'});
+    ScrollReveal().reveal('.about_wrapper,.img_fluid',{origin:'left'});
+    ScrollReveal().reveal('.about_wrapper,.para',{origin:'right'});
